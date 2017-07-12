34dd19a6a7c48b44c2b73390c0174c7484d2588d
Daniël de Kok <me@danieldk.eu>
2017-07-12 16:42:59 +0200

    Add benchmark for feature map construction.

---
+++ benches/features.rs
@@ -0,0 +1,28 @@
+#![feature(test)]
+
+extern crate conllx;
+
+extern crate test;
+
+use test::{Bencher, black_box};
+
+use conllx::Features;
+
+static FEATURES: &'static [&'static str] = &[
+    "cat:regular+noun|case:nominative|number:plural",
+    "cat:article|definite:true|case:nominative|number:singular|gender:feminine",
+    "cat:proper+name|case:nominative|number:singular|gender:feminine",
+    "cat:regular+noun|case:nominative|number:singular|case:neuter",
+    "cat:symbol|cat:punctuation",
+];
+
+#[bench]
+pub fn bench_as_map(b: &mut Bencher) {
+    b.iter(|| for f_str in FEATURES {
+        let features = Features::from_string(*f_str);
+        black_box(features.as_map());
+        black_box(features.as_map());
+        black_box(features.as_map());
+        black_box(features.as_map());
+    });
+}
